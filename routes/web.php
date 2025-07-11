770c97b2d4b07ed605a21b90fba57382a8a5b4e0
Mahendran Lavan <mahendranlavan29@gmail.com>
2025-07-11 20:42:27 +0530

    admin-panel

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.login');
+    return view('layouts.admin_layout');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +29,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/products', function () {
+    return view('admin.products');
+})->name('admin.products');
